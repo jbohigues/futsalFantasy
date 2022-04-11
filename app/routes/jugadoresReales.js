f432b5cecf58b74a60a7e7851c074e37edca6a5c
Juan Bohigues <jabohigues@iesfuentesanluis.org>
2022-04-11 22:04:59 +0200

    terminando alineacion

--- app/routes/jugadoresReales.js
+++ app/routes/jugadoresReales.js
@@ -11,12 +11,28 @@ router.get("/idE=:id", async function (req, res, next) {
     const jugadores = await prisma.jugadoresreales.findMany({
       where: {
         idEquipoUsuario: id
-      }
+      }, 
+      orderBy: [
+        {posicion: 'asc'},
+        {puntos: 'desc'}
+      ]
     });
     res.json(jugadores);
   } catch (error) {
     res.send("ERROR: " + error);
   }
+});
+
+router.put("/update/id=:idJugadorReal", async function (req, res) {
+  const { titular } = req.body;
+  const jugador = await prisma.jugadoresreales.update({
+    where: { id: Number(req.params.idJugadorReal) },
+    data: {
+      titular: titular
+    },
+  });
+  let status = jugador == null ? "mal" : "update";
+  res.json({ jugador: jugador, status: status });
 });
 
 module.exports = router;
